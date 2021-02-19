26aac198ea25b54852bb15e89433385d85c4106b
chaitanyapratap19 <54471940+chaitanyapratap19@users.noreply.github.com>
2021-02-19 18:40:07 +0530

    Update Hook.java

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -31,8 +31,9 @@ public class Hook extends BaseUtil{
         //Chrome driver
         //System.setProperty("webdriver.chrome.driver", "C:\\Libs\\chromedriver.exe");
         System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
-        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
         options.addArguments("--no-sandbox"); // Bypass OS security model
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+       
         base.Driver = new ChromeDriver();
     }
 
